573de5f8a327b451382a10e789191b7a994ea3b1
jinwoo park <elvintern@gmail.com>
2023-09-08 14:06:55 +1200

    Add config and connect DB

---
+++ backend/backend/Controllers/CompanyController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private ApplicationDbContext _context { get; }
+        public CompanyController(ApplicationDbContext context)
+        {
+
+            _context = context;
+
+        }
+
+
+        // CRUD
+
+        // Create
+
+        // Read
+
+        // Update
+
+        // Delete
+    }
+}
--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,8 +1,19 @@
+using backend.Controllers;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+
+//DB Configuration
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("local"));
+});
+
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
